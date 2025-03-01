cef27188a3266d1874eb381d77c94a2bd228186b
sandisya <143463315+sandisya@users.noreply.github.com>
2025-03-01 17:12:30 +0700

    Pemrograman Desktop

---
+++ FormMahasiswa/Form1.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtnip = New System.Windows.Forms.TextBox()
+        Me.txtnama = New System.Windows.Forms.TextBox()
+        Me.cbfakultas = New System.Windows.Forms.ComboBox()
+        Me.cbjurusan = New System.Windows.Forms.ComboBox()
+        Me.btnprocess = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(38, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(30, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "NIP"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(38, 61)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(45, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Nama"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(38, 97)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(61, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Fakultas"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(38, 136)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(59, 17)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Jurusan"
+        '
+        'txtnip
+        '
+        Me.txtnip.Location = New System.Drawing.Point(115, 24)
+        Me.txtnip.Name = "txtnip"
+        Me.txtnip.Size = New System.Drawing.Size(179, 22)
+        Me.txtnip.TabIndex = 4
+        '
+        'txtnama
+        '
+        Me.txtnama.Location = New System.Drawing.Point(115, 58)
+        Me.txtnama.Name = "txtnama"
+        Me.txtnama.Size = New System.Drawing.Size(251, 22)
+        Me.txtnama.TabIndex = 5
+        '
+        'cbfakultas
+        '
+        Me.cbfakultas.FormattingEnabled = True
+        Me.cbfakultas.Items.AddRange(New Object() {"FMIPA", "FKIP", "FEB"})
+        Me.cbfakultas.Location = New System.Drawing.Point(115, 90)
+        Me.cbfakultas.Name = "cbfakultas"
+        Me.cbfakultas.Size = New System.Drawing.Size(179, 24)
+        Me.cbfakultas.TabIndex = 6
+        '
+        'cbjurusan
+        '
+        Me.cbjurusan.FormattingEnabled = True
+        Me.cbjurusan.Items.AddRange(New Object() {"MATEMATIKA", "KIMIA", "BIOLOGI", "FISIKA", "ILMU KOMPUTER"})
+        Me.cbjurusan.Location = New System.Drawing.Point(115, 133)
+        Me.cbjurusan.Name = "cbjurusan"
+        Me.cbjurusan.Size = New System.Drawing.Size(179, 24)
+        Me.cbjurusan.TabIndex = 7
+        '
+        'btnprocess
+        '
+        Me.btnprocess.Location = New System.Drawing.Point(115, 189)
+        Me.btnprocess.Name = "btnprocess"
+        Me.btnprocess.Size = New System.Drawing.Size(75, 23)
+        Me.btnprocess.TabIndex = 8
+        Me.btnprocess.Text = "Process"
+        Me.btnprocess.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(544, 270)
+        Me.Controls.Add(Me.btnprocess)
+        Me.Controls.Add(Me.cbjurusan)
+        Me.Controls.Add(Me.cbfakultas)
+        Me.Controls.Add(Me.txtnama)
+        Me.Controls.Add(Me.txtnip)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtnip As System.Windows.Forms.TextBox
+    Friend WithEvents txtnama As System.Windows.Forms.TextBox
+    Friend WithEvents cbfakultas As System.Windows.Forms.ComboBox
+    Friend WithEvents cbjurusan As System.Windows.Forms.ComboBox
+    Friend WithEvents btnprocess As System.Windows.Forms.Button
+
+End Class
---
+++ FormMahasiswa/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+
+    Private Sub btnprocess_Click(sender As Object, e As EventArgs) Handles btnprocess.Click
+        Dim nip As Long
+        If Not Long.TryParse(txtnip.Text, nip) Then
+            MessageBox.Show("NIP harus berupa angka!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End If
+        Dim nama As String = txtnama.Text
+        Dim fakultas As String = cbfakultas.Text
+        Dim jurusan As String = cbjurusan.Text
+
+        MessageBox.Show("Hai : " & nama & vbCrLf &
+                        "NIP : " & nip & vbCrLf &
+                        "Fakultas : " & fakultas & vbCrLf &
+                        "Jurusan : " & jurusan,
+                        "Informasi",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information)
+    End Sub
+End Class
