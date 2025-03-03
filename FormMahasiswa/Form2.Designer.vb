4b71134411420feaf88f3a7c7b817e3a5283226a
sandisya <143463315+sandisya@users.noreply.github.com>
2025-03-03 21:29:46 +0700

    Pemrograman Desktop

---
+++ FormMahasiswa/Form2.Designer.vb
@@ -0,0 +1,182 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.btnprocess = New System.Windows.Forms.Button()
+        Me.txtnip = New System.Windows.Forms.TextBox()
+        Me.txtnama = New System.Windows.Forms.TextBox()
+        Me.rbL = New System.Windows.Forms.RadioButton()
+        Me.rbP = New System.Windows.Forms.RadioButton()
+        Me.cbfakultas = New System.Windows.Forms.ComboBox()
+        Me.cbjurusan = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(35, 43)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(30, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "NIP"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(35, 97)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(45, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Nama"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(35, 149)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(95, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Jenis Kelamin"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(35, 201)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(61, 17)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Fakultas"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(35, 259)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(59, 17)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Jurusan"
+        '
+        'btnprocess
+        '
+        Me.btnprocess.Location = New System.Drawing.Point(38, 294)
+        Me.btnprocess.Name = "btnprocess"
+        Me.btnprocess.Size = New System.Drawing.Size(75, 23)
+        Me.btnprocess.TabIndex = 5
+        Me.btnprocess.Text = "Process"
+        Me.btnprocess.UseVisualStyleBackColor = True
+        '
+        'txtnip
+        '
+        Me.txtnip.Location = New System.Drawing.Point(141, 40)
+        Me.txtnip.Name = "txtnip"
+        Me.txtnip.Size = New System.Drawing.Size(170, 22)
+        Me.txtnip.TabIndex = 6
+        '
+        'txtnama
+        '
+        Me.txtnama.Location = New System.Drawing.Point(141, 94)
+        Me.txtnama.Name = "txtnama"
+        Me.txtnama.Size = New System.Drawing.Size(265, 22)
+        Me.txtnama.TabIndex = 7
+        '
+        'rbL
+        '
+        Me.rbL.AutoSize = True
+        Me.rbL.Location = New System.Drawing.Point(141, 147)
+        Me.rbL.Name = "rbL"
+        Me.rbL.Size = New System.Drawing.Size(86, 21)
+        Me.rbL.TabIndex = 8
+        Me.rbL.TabStop = True
+        Me.rbL.Text = "Laki-Laki"
+        Me.rbL.UseVisualStyleBackColor = True
+        '
+        'rbP
+        '
+        Me.rbP.AutoSize = True
+        Me.rbP.Location = New System.Drawing.Point(233, 147)
+        Me.rbP.Name = "rbP"
+        Me.rbP.Size = New System.Drawing.Size(102, 21)
+        Me.rbP.TabIndex = 9
+        Me.rbP.TabStop = True
+        Me.rbP.Text = "Perempuan"
+        Me.rbP.UseVisualStyleBackColor = True
+        '
+        'cbfakultas
+        '
+        Me.cbfakultas.FormattingEnabled = True
+        Me.cbfakultas.Items.AddRange(New Object() {"FMIPA", "FKIP", "FEB"})
+        Me.cbfakultas.Location = New System.Drawing.Point(141, 198)
+        Me.cbfakultas.Name = "cbfakultas"
+        Me.cbfakultas.Size = New System.Drawing.Size(121, 24)
+        Me.cbfakultas.TabIndex = 10
+        '
+        'cbjurusan
+        '
+        Me.cbjurusan.FormattingEnabled = True
+        Me.cbjurusan.Items.AddRange(New Object() {"Matematika", "Kimia", "Biologi", "Fisika", "Ilmu Komputer"})
+        Me.cbjurusan.Location = New System.Drawing.Point(141, 256)
+        Me.cbjurusan.Name = "cbjurusan"
+        Me.cbjurusan.Size = New System.Drawing.Size(121, 24)
+        Me.cbjurusan.TabIndex = 11
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(561, 345)
+        Me.Controls.Add(Me.cbjurusan)
+        Me.Controls.Add(Me.cbfakultas)
+        Me.Controls.Add(Me.rbP)
+        Me.Controls.Add(Me.rbL)
+        Me.Controls.Add(Me.txtnama)
+        Me.Controls.Add(Me.txtnip)
+        Me.Controls.Add(Me.btnprocess)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents btnprocess As System.Windows.Forms.Button
+    Friend WithEvents txtnip As System.Windows.Forms.TextBox
+    Friend WithEvents txtnama As System.Windows.Forms.TextBox
+    Friend WithEvents rbL As System.Windows.Forms.RadioButton
+    Friend WithEvents rbP As System.Windows.Forms.RadioButton
+    Friend WithEvents cbfakultas As System.Windows.Forms.ComboBox
+    Friend WithEvents cbjurusan As System.Windows.Forms.ComboBox
+End Class
---
+++ FormMahasiswa/Form2.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form2
+
+    Private Sub btnprocess_Click(sender As Object, e As EventArgs) Handles btnprocess.Click
+        Dim nip As Long
+        If Not Long.TryParse(txtnip.Text.Trim(), nip) Then
+            MessageBox.Show("NIP harus berupa angka!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End If
+
+        Dim nama As String = txtnama.Text.Trim()
+        Dim fakultas As String = cbfakultas.Text.Trim()
+        Dim jurusan As String = cbjurusan.Text.Trim()
+
+        Dim jenisKelamin As String = ""
+        If rbL.Checked Then
+            jenisKelamin = "Laki-Laki"
+        ElseIf rbP.Checked Then
+            jenisKelamin = "Perempuan"
+        Else
+            MessageBox.Show("Silakan pilih jenis kelamin!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Exit Sub
+        End If
+
+        MessageBox.Show("Hai : " & nama & vbCrLf &
+                        "NIP : " & nip & vbCrLf &
+                        "Jenis Kelamin : " & jenisKelamin & vbCrLf &
+                        "Fakultas : " & fakultas & vbCrLf &
+                        "Jurusan : " & jurusan,
+                        "Informasi",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information)
+    End Sub
+
+End Class
