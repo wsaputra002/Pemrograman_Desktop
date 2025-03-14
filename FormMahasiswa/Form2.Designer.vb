755456efaa99a27033009170cc8c935e23d96627
sandisya <143463315+sandisya@users.noreply.github.com>
2025-03-14 16:40:34 +0700

    If Else dan Select Case

--- FormMahasiswa/Form2.Designer.vb
+++ FormMahasiswa/Form2.Designer.vb
@@ -34,13 +34,25 @@ Partial Class Form2
         Me.rbP = New System.Windows.Forms.RadioButton()
         Me.cbfakultas = New System.Windows.Forms.ComboBox()
         Me.cbjurusan = New System.Windows.Forms.ComboBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txttugas = New System.Windows.Forms.TextBox()
+        Me.txtuts = New System.Windows.Forms.TextBox()
+        Me.txtuas = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(33, 46)
+        Me.Label1.Location = New System.Drawing.Point(13, 12)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(44, 25)
         Me.Label1.TabIndex = 0
@@ -50,7 +62,7 @@ Partial Class Form2
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(33, 100)
+        Me.Label2.Location = New System.Drawing.Point(13, 66)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(64, 25)
         Me.Label2.TabIndex = 1
@@ -60,7 +72,7 @@ Partial Class Form2
         '
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(33, 147)
+        Me.Label3.Location = New System.Drawing.Point(13, 113)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(135, 25)
         Me.Label3.TabIndex = 2
@@ -70,7 +82,7 @@ Partial Class Form2
         '
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(35, 201)
+        Me.Label4.Location = New System.Drawing.Point(15, 167)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(103, 29)
         Me.Label4.TabIndex = 3
@@ -80,7 +92,7 @@ Partial Class Form2
         '
         Me.Label5.AutoSize = True
         Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label5.Location = New System.Drawing.Point(35, 259)
+        Me.Label5.Location = New System.Drawing.Point(15, 225)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(97, 29)
         Me.Label5.TabIndex = 4
@@ -89,7 +101,7 @@ Partial Class Form2
         'btnprocess
         '
         Me.btnprocess.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnprocess.Location = New System.Drawing.Point(38, 310)
+        Me.btnprocess.Location = New System.Drawing.Point(3, 3)
         Me.btnprocess.Name = "btnprocess"
         Me.btnprocess.Size = New System.Drawing.Size(130, 34)
         Me.btnprocess.TabIndex = 5
@@ -99,7 +111,7 @@ Partial Class Form2
         'txtnip
         '
         Me.txtnip.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtnip.Location = New System.Drawing.Point(141, 40)
+        Me.txtnip.Location = New System.Drawing.Point(121, 6)
         Me.txtnip.Name = "txtnip"
         Me.txtnip.Size = New System.Drawing.Size(252, 34)
         Me.txtnip.TabIndex = 6
@@ -107,7 +119,7 @@ Partial Class Form2
         'txtnama
         '
         Me.txtnama.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtnama.Location = New System.Drawing.Point(141, 94)
+        Me.txtnama.Location = New System.Drawing.Point(121, 60)
         Me.txtnama.Name = "txtnama"
         Me.txtnama.Size = New System.Drawing.Size(408, 34)
         Me.txtnama.TabIndex = 7
@@ -116,7 +128,7 @@ Partial Class Form2
         '
         Me.rbL.AutoSize = True
         Me.rbL.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.rbL.Location = New System.Drawing.Point(174, 151)
+        Me.rbL.Location = New System.Drawing.Point(154, 117)
         Me.rbL.Name = "rbL"
         Me.rbL.Size = New System.Drawing.Size(98, 24)
         Me.rbL.TabIndex = 8
@@ -128,7 +140,7 @@ Partial Class Form2
         '
         Me.rbP.AutoSize = True
         Me.rbP.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.rbP.Location = New System.Drawing.Point(278, 152)
+        Me.rbP.Location = New System.Drawing.Point(258, 118)
         Me.rbP.Name = "rbP"
         Me.rbP.Size = New System.Drawing.Size(115, 24)
         Me.rbP.TabIndex = 9
@@ -138,45 +150,138 @@ Partial Class Form2
         '
         'cbfakultas
         '
+        Me.cbfakultas.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.cbfakultas.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.cbfakultas.FormattingEnabled = True
-        Me.cbfakultas.Items.AddRange(New Object() {"FMIPA", "FKIP", "FEB"})
-        Me.cbfakultas.Location = New System.Drawing.Point(141, 198)
+        Me.cbfakultas.Location = New System.Drawing.Point(121, 164)
         Me.cbfakultas.Name = "cbfakultas"
-        Me.cbfakultas.Size = New System.Drawing.Size(252, 37)
+        Me.cbfakultas.Size = New System.Drawing.Size(408, 37)
         Me.cbfakultas.TabIndex = 10
         '
         'cbjurusan
         '
+        Me.cbjurusan.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.cbjurusan.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.cbjurusan.FormattingEnabled = True
-        Me.cbjurusan.Items.AddRange(New Object() {"Matematika", "Kimia", "Biologi", "Fisika", "Ilmu Komputer"})
-        Me.cbjurusan.Location = New System.Drawing.Point(141, 256)
+        Me.cbjurusan.Location = New System.Drawing.Point(121, 222)
         Me.cbjurusan.Name = "cbjurusan"
-        Me.cbjurusan.Size = New System.Drawing.Size(252, 37)
+        Me.cbjurusan.Size = New System.Drawing.Size(408, 37)
         Me.cbjurusan.TabIndex = 11
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Label10)
+        Me.Panel1.Controls.Add(Me.Label9)
+        Me.Panel1.Controls.Add(Me.Label8)
+        Me.Panel1.Controls.Add(Me.Label7)
+        Me.Panel1.Controls.Add(Me.txttugas)
+        Me.Panel1.Controls.Add(Me.txtuts)
+        Me.Panel1.Controls.Add(Me.txtuas)
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Controls.Add(Me.cbfakultas)
+        Me.Panel1.Controls.Add(Me.cbjurusan)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.rbP)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.rbL)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.txtnama)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.txtnip)
+        Me.Panel1.Location = New System.Drawing.Point(13, 22)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(662, 337)
+        Me.Panel1.TabIndex = 12
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(429, 292)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(36, 17)
+        Me.Label9.TabIndex = 18
+        Me.Label9.Text = "UAS"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(283, 292)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(36, 17)
+        Me.Label8.TabIndex = 17
+        Me.Label8.Text = "UTS"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(120, 292)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(48, 17)
+        Me.Label7.TabIndex = 16
+        Me.Label7.Text = "Tugas"
+        '
+        'txttugas
+        '
+        Me.txttugas.Location = New System.Drawing.Point(177, 289)
+        Me.txttugas.Name = "txttugas"
+        Me.txttugas.Size = New System.Drawing.Size(100, 22)
+        Me.txttugas.TabIndex = 13
+        '
+        'txtuts
+        '
+        Me.txtuts.Location = New System.Drawing.Point(323, 289)
+        Me.txtuts.Name = "txtuts"
+        Me.txtuts.Size = New System.Drawing.Size(100, 22)
+        Me.txtuts.TabIndex = 14
+        '
+        'txtuas
+        '
+        Me.txtuas.Location = New System.Drawing.Point(466, 289)
+        Me.txtuas.Name = "txtuas"
+        Me.txtuas.Size = New System.Drawing.Size(100, 22)
+        Me.txtuas.TabIndex = 15
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(15, 285)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(49, 25)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Nilai"
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.btnprocess)
+        Me.Panel2.Location = New System.Drawing.Point(12, 381)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(662, 41)
+        Me.Panel2.TabIndex = 13
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(572, 293)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(75, 17)
+        Me.Label10.TabIndex = 19
+        Me.Label10.Text = "- GRADE -"
         '
         'Form2
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(561, 358)
-        Me.Controls.Add(Me.cbjurusan)
-        Me.Controls.Add(Me.cbfakultas)
-        Me.Controls.Add(Me.rbP)
-        Me.Controls.Add(Me.rbL)
-        Me.Controls.Add(Me.txtnama)
-        Me.Controls.Add(Me.txtnip)
-        Me.Controls.Add(Me.btnprocess)
-        Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
+        Me.ClientSize = New System.Drawing.Size(696, 441)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
         Me.Name = "Form2"
         Me.Text = "Form2"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
     Friend WithEvents Label1 As System.Windows.Forms.Label
@@ -191,4 +296,14 @@ Partial Class Form2
     Friend WithEvents rbP As System.Windows.Forms.RadioButton
     Friend WithEvents cbfakultas As System.Windows.Forms.ComboBox
     Friend WithEvents cbjurusan As System.Windows.Forms.ComboBox
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents txttugas As System.Windows.Forms.TextBox
+    Friend WithEvents txtuts As System.Windows.Forms.TextBox
+    Friend WithEvents txtuas As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents Label10 As System.Windows.Forms.Label
 End Class
--- FormMahasiswa/Form2.vb
+++ FormMahasiswa/Form2.vb
@@ -1,5 +1,19 @@
 ﻿Public Class Form2
 
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cbfakultas.Items.Add("FMIPA")
+        cbfakultas.Items.Add("FKIP")
+        cbfakultas.Items.Add("FEB")
+    End Sub
+
+    Private Sub txtnip_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtnip.KeyPress
+        If Asc(e.KeyChar) <> 8 Then
+            If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
+                e.Handled = True
+            End If
+        End If
+    End Sub
+
     Private Sub btnprocess_Click(sender As Object, e As EventArgs) Handles btnprocess.Click
         Dim nip As Long
         If Not Long.TryParse(txtnip.Text.Trim(), nip) Then
@@ -8,6 +22,25 @@
         Dim nama As String = txtnama.Text.Trim()
         Dim fakultas As String = cbfakultas.Text.Trim()
         Dim jurusan As String = cbjurusan.Text.Trim()
+        Dim tugas As Double = Double.Parse(txttugas.Text)
+        Dim uts As Double = Double.Parse(txtuts.Text)
+        Dim uas As Double = Double.Parse(txtuas.Text)
+
+        Dim total As Double = (tugas * 0.3) + (uts * 0.3) + (uas * 0.4)
+
+        Dim grade As String
+        Select Case total
+            Case Is >= 78
+                grade = "A"
+            Case Is >= 65
+                grade = "B"
+            Case Is >= 50
+                grade = "C"
+            Case Is >= 40
+                grade = "D"
+            Case Else
+                grade = "E"
+        End Select
 
         Dim jenisKelamin As String = ""
         If rbL.Checked Then
@@ -23,17 +56,29 @@
                         "NIP : " & nip & vbCrLf &
                         "Jenis Kelamin : " & jenisKelamin & vbCrLf &
                         "Fakultas : " & fakultas & vbCrLf &
-                        "Jurusan : " & jurusan,
+                        "Jurusan : " & jurusan & vbCrLf &
+                        "Nilai Akhir: " & total.ToString("F2") & vbCrLf &
+                        "GRADE: " & grade,
                         "Informasi",
                         MessageBoxButtons.OK,
                         MessageBoxIcon.Information)
     End Sub
 
-    Private Sub txtnip_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtnip.KeyPress
-        If Asc(e.KeyChar) <> 8 Then
-            If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
-                e.Handled = True
-            End If
-        End If
+    Private Sub cbfakultas_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbfakultas.SelectedIndexChanged
+        Dim fakultas As String = cbfakultas.SelectedItem
+        cbjurusan.Items.Clear()
+        Select Case (fakultas)
+            Case "FMIPA"
+                cbjurusan.Items.Add("Sistem Informasi")
+                cbjurusan.Items.Add("Manajemen Informatika")
+                cbjurusan.Items.Add("Ilmu Komputer")
+            Case "FKIP"
+                cbjurusan.Items.Add("Pendidikan Teknologi Informasi")
+                cbjurusan.Items.Add("Pendidikan Jasmani")
+            Case "FEB"
+                cbjurusan.Items.Add("Manajemen")
+                cbjurusan.Items.Add("Perbankan")
+                cbjurusan.Items.Add("Akuntansi")
+        End Select
     End Sub
 End Class
