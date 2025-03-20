0187e53275e9a2269006025480bc3a3b7cdce336
sandisya <143463315+sandisya@users.noreply.github.com>
2025-03-20 08:23:44 +0700

    Tugas terakhir sebelum UTS

--- FormMahasiswa/Form2.Designer.vb
+++ FormMahasiswa/Form2.Designer.vb
@@ -35,6 +35,7 @@ Partial Class Form2
         Me.cbfakultas = New System.Windows.Forms.ComboBox()
         Me.cbjurusan = New System.Windows.Forms.ComboBox()
         Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lblgrade = New System.Windows.Forms.Label()
         Me.Label9 = New System.Windows.Forms.Label()
         Me.Label8 = New System.Windows.Forms.Label()
         Me.Label7 = New System.Windows.Forms.Label()
@@ -43,16 +44,29 @@ Partial Class Form2
         Me.txtuas = New System.Windows.Forms.TextBox()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.Panel2 = New System.Windows.Forms.Panel()
-        Me.Label10 = New System.Windows.Forms.Label()
+        Me.dgvdata = New System.Windows.Forms.DataGridView()
+        Me.dgnip = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.dgnama = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.dgjeniskelamin = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.dgjurusan = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.dgmatkul = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.dggrade = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.btnclose = New System.Windows.Forms.Button()
+        Me.btndelete = New System.Windows.Forms.Button()
+        Me.btnsave = New System.Windows.Forms.Button()
+        Me.btnnew = New System.Windows.Forms.Button()
         Me.Panel1.SuspendLayout()
         Me.Panel2.SuspendLayout()
+        CType(Me.dgvdata, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel3.SuspendLayout()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(13, 12)
+        Me.Label1.Location = New System.Drawing.Point(20, 12)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(44, 25)
         Me.Label1.TabIndex = 0
@@ -62,7 +76,7 @@ Partial Class Form2
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(13, 66)
+        Me.Label2.Location = New System.Drawing.Point(20, 66)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(64, 25)
         Me.Label2.TabIndex = 1
@@ -72,7 +86,7 @@ Partial Class Form2
         '
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(13, 113)
+        Me.Label3.Location = New System.Drawing.Point(20, 115)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(135, 25)
         Me.Label3.TabIndex = 2
@@ -82,7 +96,7 @@ Partial Class Form2
         '
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(15, 167)
+        Me.Label4.Location = New System.Drawing.Point(20, 167)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(103, 29)
         Me.Label4.TabIndex = 3
@@ -92,7 +106,7 @@ Partial Class Form2
         '
         Me.Label5.AutoSize = True
         Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label5.Location = New System.Drawing.Point(15, 225)
+        Me.Label5.Location = New System.Drawing.Point(20, 225)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(97, 29)
         Me.Label5.TabIndex = 4
@@ -100,12 +114,12 @@ Partial Class Form2
         '
         'btnprocess
         '
+        Me.btnprocess.Enabled = False
         Me.btnprocess.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnprocess.Location = New System.Drawing.Point(3, 3)
+        Me.btnprocess.Location = New System.Drawing.Point(415, 110)
         Me.btnprocess.Name = "btnprocess"
-        Me.btnprocess.Size = New System.Drawing.Size(130, 34)
+        Me.btnprocess.Size = New System.Drawing.Size(245, 34)
         Me.btnprocess.TabIndex = 5
-        Me.btnprocess.Text = "Process"
         Me.btnprocess.UseVisualStyleBackColor = True
         '
         'txtnip
@@ -113,7 +127,7 @@ Partial Class Form2
         Me.txtnip.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.txtnip.Location = New System.Drawing.Point(121, 6)
         Me.txtnip.Name = "txtnip"
-        Me.txtnip.Size = New System.Drawing.Size(252, 34)
+        Me.txtnip.Size = New System.Drawing.Size(539, 34)
         Me.txtnip.TabIndex = 6
         '
         'txtnama
@@ -121,14 +135,14 @@ Partial Class Form2
         Me.txtnama.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.txtnama.Location = New System.Drawing.Point(121, 60)
         Me.txtnama.Name = "txtnama"
-        Me.txtnama.Size = New System.Drawing.Size(408, 34)
+        Me.txtnama.Size = New System.Drawing.Size(539, 34)
         Me.txtnama.TabIndex = 7
         '
         'rbL
         '
         Me.rbL.AutoSize = True
         Me.rbL.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.rbL.Location = New System.Drawing.Point(154, 117)
+        Me.rbL.Location = New System.Drawing.Point(175, 115)
         Me.rbL.Name = "rbL"
         Me.rbL.Size = New System.Drawing.Size(98, 24)
         Me.rbL.TabIndex = 8
@@ -140,7 +154,7 @@ Partial Class Form2
         '
         Me.rbP.AutoSize = True
         Me.rbP.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.rbP.Location = New System.Drawing.Point(258, 118)
+        Me.rbP.Location = New System.Drawing.Point(282, 115)
         Me.rbP.Name = "rbP"
         Me.rbP.Size = New System.Drawing.Size(115, 24)
         Me.rbP.TabIndex = 9
@@ -155,7 +169,7 @@ Partial Class Form2
         Me.cbfakultas.FormattingEnabled = True
         Me.cbfakultas.Location = New System.Drawing.Point(121, 164)
         Me.cbfakultas.Name = "cbfakultas"
-        Me.cbfakultas.Size = New System.Drawing.Size(408, 37)
+        Me.cbfakultas.Size = New System.Drawing.Size(539, 37)
         Me.cbfakultas.TabIndex = 10
         '
         'cbjurusan
@@ -165,12 +179,13 @@ Partial Class Form2
         Me.cbjurusan.FormattingEnabled = True
         Me.cbjurusan.Location = New System.Drawing.Point(121, 222)
         Me.cbjurusan.Name = "cbjurusan"
-        Me.cbjurusan.Size = New System.Drawing.Size(408, 37)
+        Me.cbjurusan.Size = New System.Drawing.Size(539, 37)
         Me.cbjurusan.TabIndex = 11
         '
         'Panel1
         '
-        Me.Panel1.Controls.Add(Me.Label10)
+        Me.Panel1.Controls.Add(Me.btnprocess)
+        Me.Panel1.Controls.Add(Me.lblgrade)
         Me.Panel1.Controls.Add(Me.Label9)
         Me.Panel1.Controls.Add(Me.Label8)
         Me.Panel1.Controls.Add(Me.Label7)
@@ -191,13 +206,23 @@ Partial Class Form2
         Me.Panel1.Controls.Add(Me.txtnip)
         Me.Panel1.Location = New System.Drawing.Point(13, 22)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(662, 337)
+        Me.Panel1.Size = New System.Drawing.Size(834, 337)
         Me.Panel1.TabIndex = 12
+        '
+        'lblgrade
+        '
+        Me.lblgrade.AutoSize = True
+        Me.lblgrade.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblgrade.Location = New System.Drawing.Point(647, 288)
+        Me.lblgrade.Name = "lblgrade"
+        Me.lblgrade.Size = New System.Drawing.Size(100, 20)
+        Me.lblgrade.TabIndex = 19
+        Me.lblgrade.Text = "- GRADE -"
         '
         'Label9
         '
         Me.Label9.AutoSize = True
-        Me.Label9.Location = New System.Drawing.Point(429, 292)
+        Me.Label9.Location = New System.Drawing.Point(463, 287)
         Me.Label9.Name = "Label9"
         Me.Label9.Size = New System.Drawing.Size(36, 17)
         Me.Label9.TabIndex = 18
@@ -206,7 +231,7 @@ Partial Class Form2
         'Label8
         '
         Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(283, 292)
+        Me.Label8.Location = New System.Drawing.Point(300, 287)
         Me.Label8.Name = "Label8"
         Me.Label8.Size = New System.Drawing.Size(36, 17)
         Me.Label8.TabIndex = 17
@@ -215,7 +240,7 @@ Partial Class Form2
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(120, 292)
+        Me.Label7.Location = New System.Drawing.Point(121, 289)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(48, 17)
         Me.Label7.TabIndex = 16
@@ -223,30 +248,30 @@ Partial Class Form2
         '
         'txttugas
         '
-        Me.txttugas.Location = New System.Drawing.Point(177, 289)
+        Me.txttugas.Location = New System.Drawing.Point(175, 284)
         Me.txttugas.Name = "txttugas"
-        Me.txttugas.Size = New System.Drawing.Size(100, 22)
+        Me.txttugas.Size = New System.Drawing.Size(119, 22)
         Me.txttugas.TabIndex = 13
         '
         'txtuts
         '
-        Me.txtuts.Location = New System.Drawing.Point(323, 289)
+        Me.txtuts.Location = New System.Drawing.Point(342, 284)
         Me.txtuts.Name = "txtuts"
-        Me.txtuts.Size = New System.Drawing.Size(100, 22)
+        Me.txtuts.Size = New System.Drawing.Size(115, 22)
         Me.txtuts.TabIndex = 14
         '
         'txtuas
         '
-        Me.txtuas.Location = New System.Drawing.Point(466, 289)
+        Me.txtuas.Location = New System.Drawing.Point(505, 284)
         Me.txtuas.Name = "txtuas"
-        Me.txtuas.Size = New System.Drawing.Size(100, 22)
+        Me.txtuas.Size = New System.Drawing.Size(122, 22)
         Me.txtuas.TabIndex = 15
         '
         'Label6
         '
         Me.Label6.AutoSize = True
         Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label6.Location = New System.Drawing.Point(15, 285)
+        Me.Label6.Location = New System.Drawing.Point(20, 284)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(49, 25)
         Me.Label6.TabIndex = 12
@@ -254,26 +279,110 @@ Partial Class Form2
         '
         'Panel2
         '
-        Me.Panel2.Controls.Add(Me.btnprocess)
-        Me.Panel2.Location = New System.Drawing.Point(12, 381)
+        Me.Panel2.Controls.Add(Me.dgvdata)
+        Me.Panel2.Location = New System.Drawing.Point(12, 440)
         Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(662, 41)
+        Me.Panel2.Size = New System.Drawing.Size(840, 153)
         Me.Panel2.TabIndex = 13
         '
-        'Label10
+        'dgvdata
         '
-        Me.Label10.AutoSize = True
-        Me.Label10.Location = New System.Drawing.Point(572, 293)
-        Me.Label10.Name = "Label10"
-        Me.Label10.Size = New System.Drawing.Size(75, 17)
-        Me.Label10.TabIndex = 19
-        Me.Label10.Text = "- GRADE -"
+        Me.dgvdata.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvdata.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.dgnip, Me.dgnama, Me.dgjeniskelamin, Me.dgjurusan, Me.dgmatkul, Me.dggrade})
+        Me.dgvdata.Location = New System.Drawing.Point(3, 3)
+        Me.dgvdata.Name = "dgvdata"
+        Me.dgvdata.RowTemplate.Height = 24
+        Me.dgvdata.Size = New System.Drawing.Size(832, 147)
+        Me.dgvdata.TabIndex = 4
+        '
+        'dgnip
+        '
+        Me.dgnip.HeaderText = "NIP"
+        Me.dgnip.Name = "dgnip"
+        '
+        'dgnama
+        '
+        Me.dgnama.HeaderText = "Nama"
+        Me.dgnama.Name = "dgnama"
+        '
+        'dgjeniskelamin
+        '
+        Me.dgjeniskelamin.HeaderText = "Jenis Kelamin"
+        Me.dgjeniskelamin.Name = "dgjeniskelamin"
+        '
+        'dgjurusan
+        '
+        Me.dgjurusan.HeaderText = "Jurusan"
+        Me.dgjurusan.Name = "dgjurusan"
+        '
+        'dgmatkul
+        '
+        Me.dgmatkul.HeaderText = "Mata Kuliah"
+        Me.dgmatkul.Name = "dgmatkul"
+        '
+        'dggrade
+        '
+        Me.dggrade.HeaderText = "GRADE"
+        Me.dggrade.Name = "dggrade"
+        '
+        'Panel3
+        '
+        Me.Panel3.Controls.Add(Me.btnclose)
+        Me.Panel3.Controls.Add(Me.btndelete)
+        Me.Panel3.Controls.Add(Me.btnsave)
+        Me.Panel3.Controls.Add(Me.btnnew)
+        Me.Panel3.Location = New System.Drawing.Point(12, 365)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(835, 59)
+        Me.Panel3.TabIndex = 0
+        '
+        'btnclose
+        '
+        Me.btnclose.BackColor = System.Drawing.Color.LightCoral
+        Me.btnclose.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnclose.Location = New System.Drawing.Point(453, 16)
+        Me.btnclose.Name = "btnclose"
+        Me.btnclose.Size = New System.Drawing.Size(372, 30)
+        Me.btnclose.TabIndex = 3
+        Me.btnclose.Text = "Close"
+        Me.btnclose.UseVisualStyleBackColor = False
+        '
+        'btndelete
+        '
+        Me.btndelete.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btndelete.Location = New System.Drawing.Point(241, 16)
+        Me.btndelete.Name = "btndelete"
+        Me.btndelete.Size = New System.Drawing.Size(199, 30)
+        Me.btndelete.TabIndex = 2
+        Me.btndelete.Text = "Delete"
+        Me.btndelete.UseVisualStyleBackColor = True
+        '
+        'btnsave
+        '
+        Me.btnsave.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnsave.Location = New System.Drawing.Point(119, 16)
+        Me.btnsave.Name = "btnsave"
+        Me.btnsave.Size = New System.Drawing.Size(116, 30)
+        Me.btnsave.TabIndex = 1
+        Me.btnsave.Text = "Save"
+        Me.btnsave.UseVisualStyleBackColor = True
+        '
+        'btnnew
+        '
+        Me.btnnew.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnnew.Location = New System.Drawing.Point(3, 16)
+        Me.btnnew.Name = "btnnew"
+        Me.btnnew.Size = New System.Drawing.Size(110, 30)
+        Me.btnnew.TabIndex = 0
+        Me.btnnew.Text = "New"
+        Me.btnnew.UseVisualStyleBackColor = True
         '
         'Form2
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(696, 441)
+        Me.ClientSize = New System.Drawing.Size(867, 605)
+        Me.Controls.Add(Me.Panel3)
         Me.Controls.Add(Me.Panel2)
         Me.Controls.Add(Me.Panel1)
         Me.Name = "Form2"
@@ -281,6 +390,8 @@ Partial Class Form2
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
         Me.Panel2.ResumeLayout(False)
+        CType(Me.dgvdata, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel3.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
@@ -305,5 +416,17 @@ Partial Class Form2
     Friend WithEvents Label8 As System.Windows.Forms.Label
     Friend WithEvents Label9 As System.Windows.Forms.Label
     Friend WithEvents Panel2 As System.Windows.Forms.Panel
-    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents lblgrade As System.Windows.Forms.Label
+    Friend WithEvents dgvdata As System.Windows.Forms.DataGridView
+    Friend WithEvents dgnip As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents dgnama As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents dgjeniskelamin As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents dgjurusan As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents dgmatkul As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents dggrade As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Panel3 As System.Windows.Forms.Panel
+    Friend WithEvents btnclose As System.Windows.Forms.Button
+    Friend WithEvents btndelete As System.Windows.Forms.Button
+    Friend WithEvents btnsave As System.Windows.Forms.Button
+    Friend WithEvents btnnew As System.Windows.Forms.Button
 End Class
--- FormMahasiswa/Form2.vb
+++ FormMahasiswa/Form2.vb
@@ -6,64 +6,6 @@
         cbfakultas.Items.Add("FEB")
     End Sub
 
-    Private Sub txtnip_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtnip.KeyPress
-        If Asc(e.KeyChar) <> 8 Then
-            If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
-                e.Handled = True
-            End If
-        End If
-    End Sub
-
-    Private Sub btnprocess_Click(sender As Object, e As EventArgs) Handles btnprocess.Click
-        Dim nip As Long
-        If Not Long.TryParse(txtnip.Text.Trim(), nip) Then
-            Exit Sub
-        End If
-        Dim nama As String = txtnama.Text.Trim()
-        Dim fakultas As String = cbfakultas.Text.Trim()
-        Dim jurusan As String = cbjurusan.Text.Trim()
-        Dim tugas As Double = Double.Parse(txttugas.Text)
-        Dim uts As Double = Double.Parse(txtuts.Text)
-        Dim uas As Double = Double.Parse(txtuas.Text)
-
-        Dim total As Double = (tugas * 0.3) + (uts * 0.3) + (uas * 0.4)
-
-        Dim grade As String
-        Select Case total
-            Case Is >= 78
-                grade = "A"
-            Case Is >= 65
-                grade = "B"
-            Case Is >= 50
-                grade = "C"
-            Case Is >= 40
-                grade = "D"
-            Case Else
-                grade = "E"
-        End Select
-
-        Dim jenisKelamin As String = ""
-        If rbL.Checked Then
-            jenisKelamin = "Laki-Laki"
-        ElseIf rbP.Checked Then
-            jenisKelamin = "Perempuan"
-        Else
-            MessageBox.Show("Silakan pilih jenis kelamin!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
-            Exit Sub
-        End If
-
-        MessageBox.Show("Hai : " & nama & vbCrLf &
-                        "NIP : " & nip & vbCrLf &
-                        "Jenis Kelamin : " & jenisKelamin & vbCrLf &
-                        "Fakultas : " & fakultas & vbCrLf &
-                        "Jurusan : " & jurusan & vbCrLf &
-                        "Nilai Akhir: " & total.ToString("F2") & vbCrLf &
-                        "GRADE: " & grade,
-                        "Informasi",
-                        MessageBoxButtons.OK,
-                        MessageBoxIcon.Information)
-    End Sub
-
     Private Sub cbfakultas_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbfakultas.SelectedIndexChanged
         Dim fakultas As String = cbfakultas.SelectedItem
         cbjurusan.Items.Clear()
@@ -81,4 +23,118 @@
                 cbjurusan.Items.Add("Akuntansi")
         End Select
     End Sub
+
+    Private Sub txtnip_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtnip.KeyPress
+        If Asc(e.KeyChar) <> 8 Then
+            If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
+                e.Handled = True
+            End If
+        End If
+    End Sub
+
+    Private Sub UpdateGrade()
+        Dim tugas, uts, uas As Double
+        If Double.TryParse(txttugas.Text, tugas) AndAlso Double.TryParse(txtuts.Text, uts) AndAlso Double.TryParse(txtuas.Text, uas) Then
+            Dim total As Double = (tugas * 0.3) + (uts * 0.3) + (uas * 0.4)
+            Dim grade As String
+
+            Select Case total
+                Case Is >= 78
+                    grade = "A"
+                Case Is >= 65
+                    grade = "B"
+                Case Is >= 50
+                    grade = "C"
+                Case Is >= 40
+                    grade = "D"
+                Case Else
+                    grade = "E"
+            End Select
+
+            lblgrade.Text = " " & grade
+        Else
+            lblgrade.Text = " ... "
+        End If
+    End Sub
+
+    Private Sub txttugas_TextChanged(sender As Object, e As EventArgs) Handles txttugas.TextChanged
+        UpdateGrade()
+    End Sub
+
+    Private Sub txtuts_TextChanged(sender As Object, e As EventArgs) Handles txtuts.TextChanged
+        UpdateGrade()
+    End Sub
+
+    Private Sub txtuas_TextChanged(sender As Object, e As EventArgs) Handles txtuas.TextChanged
+        UpdateGrade()
+    End Sub
+
+    Private Sub btnnew_Click(sender As Object, e As EventArgs) Handles btnnew.Click
+        txtnip.Clear()
+        txtnama.Clear()
+        cbfakultas.SelectedIndex = -1
+        cbjurusan.SelectedIndex = -1
+        txttugas.Clear()
+        txtuts.Clear()
+        txtuas.Clear()
+        lblgrade.Text = " -GRADE- "
+        txtnip.Enabled = True
+        txtnip.Focus()
+    End Sub
+
+    Private Sub btnsave_Click(sender As Object, e As EventArgs) Handles btnsave.Click
+        If txtnip.Text = "" Or txtnama.Text = "" Or cbjurusan.Text = "" Then
+            MessageBox.Show("Harap isi semua data!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Exit Sub
+        End If
+
+        Dim rowIndex As Integer = -1
+        For Each row As DataGridViewRow In dgvdata.Rows
+            If row.Cells("dgnip").Value IsNot Nothing AndAlso row.Cells("dgnip").Value.ToString() = txtnip.Text Then
+                rowIndex = row.Index
+                Exit For
+            End If
+        Next
+
+        If rowIndex = -1 Then
+            dgvdata.Rows.Add(txtnip.Text, txtnama.Text, If(rbL.Checked, "Laki-Laki", "Perempuan"), cbjurusan.Text, "Mata Kuliah", lblgrade.Text)
+        Else
+            dgvdata.Rows(rowIndex).SetValues(txtnip.Text, txtnama.Text, If(rbL.Checked, "Laki-Laki", "Perempuan"), cbjurusan.Text, "Mata Kuliah", lblgrade.Text)
+        End If
+
+        btnnew.PerformClick()
+    End Sub
+
+    Private Sub btndelete_Click(sender As Object, e As EventArgs) Handles btndelete.Click
+        If dgvdata.SelectedRows.Count > 0 Then
+            Dim confirm As DialogResult = MessageBox.Show("Apakah yakin ingin menghapus data ini?", "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+            If confirm = DialogResult.Yes Then
+                dgvdata.Rows.RemoveAt(dgvdata.SelectedRows(0).Index)
+                btnnew.PerformClick()
+            End If
+        Else
+            MessageBox.Show("Pilih data yang ingin dihapus!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+    End Sub
+
+    Private Sub btnclose_Click(sender As Object, e As EventArgs) Handles btnclose.Click
+        Me.Close()
+    End Sub
+
+    Private Sub dgvdata_SelectionChanged(sender As Object, e As EventArgs) Handles dgvdata.SelectionChanged
+        If dgvdata.SelectedRows.Count > 0 Then
+            Dim row As DataGridViewRow = dgvdata.SelectedRows(0)
+            txtnip.Text = row.Cells("dgnip").Value.ToString()
+            txtnama.Text = row.Cells("dgnama").Value.ToString()
+            If row.Cells("dgjeniskelamin").Value.ToString() = "Laki-Laki" Then
+                rbL.Checked = True
+            Else
+                rbP.Checked = True
+            End If
+            cbjurusan.Text = row.Cells("dgjurusan").Value.ToString()
+            lblgrade.Text = row.Cells("dggrade").Value.ToString()
+            txtnip.Enabled = False
+        End If
+    End Sub
+
 End Class
